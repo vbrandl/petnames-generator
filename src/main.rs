a1ce903f9df93065ac623b1a323c2c1c54a2b93b
Valentin Brandl <mail@vbrandl.net>
2023-10-07 12:31:07 +0200

    Fix clippy lint

--- src/main.rs
+++ src/main.rs
@@ -243,8 +243,8 @@ async fn shutdown_signal() {
     let terminate = std::future::pending::<()>();
 
     tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
+        () = ctrl_c => {},
+        () = terminate => {},
     }
 
     info!("signal received, starting graceful shutdown");
